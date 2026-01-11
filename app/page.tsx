d66070605471a8048bb246106abedc62b1da0b17
RamSivaSundaraKarthykeyan <srssk2005@gmail.com>
2026-01-11 18:14:34 +0530

    Added nav bar and hero section to the main page and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { GeometricBackground } from "@/components/geomentric-background";
+import { HeroSection } from "@/components/hero-section";
+import { Navigation } from "@/components/navigation";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen overflow-x-hidden">
       <GeometricBackground/>
+      <Navigation/>
+      <HeroSection/>
     </main>    
   );
 }
